6f047d79111ec8230bec99a40cb022fad9796ee1
AliceK <wawaac@gmail.com>
2017-09-15 15:46:36 +0800

    Initial Commit

---
+++ FlightSeatPlan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FlightSeatPlan
+//
+//  Created by Alice Jin on 15/9/2017.
+//  Copyright © 2017 Alice Jin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
